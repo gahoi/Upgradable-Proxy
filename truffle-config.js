196351907a2ea66689ab211db1d3315bef908e05
Gahoi <prateek.gahoi@capgemini.com>
2021-11-30 18:20:31 +0530

    code build related changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,41 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config();
+
+
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" ,// Match any network id
-     // from:"0xc9946d79Ac322cca260047018a49cBA20Cc77a24",
-      
+
+      provider: () => new HDWalletProvider({
+        privateKeys: [process.env.abc],
+        providerOrUrl: "http://127.0.0.1:8545",
+        numberOfAddress: 1
+      }),
+      network_id: "*"
+
+    },
+    private: {
+
+      provider: () => new HDWalletProvider({
+        privateKeys: private_keys,
+        providerOrUrl: "http://3.80.41.73:8545",
+        numberOfAddress: 1
+      }),
+      network_id: "*"
+
+    }, goeril: {
+      provider: () => new HDWalletProvider({
+        privateKeys: private_keys,
+        providerOrUrl: "https://goerli.infura.io/v3/749beb03d71f432e941731c2f90a7448",
+        numberOfAddress: 1
+      }),
+      network_id: 4
     }
-  },
-  compilers: {
+  }, compilers: {
     solc: {
       version: "0.8.3"
     }
   }
+
 }
